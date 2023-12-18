8446f66cfd72b729f815c6e9c806b775adf16165
davidfung <davidfung@amgcomputing.com>
2023-12-18 08:27:45 -0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let id = menu();
+    match id {
+        1 => double_thru_channel(),
+        _ => println!("Bye"),
+    }
+}
+
+fn menu() -> i32 {
+    return 1;
+}
+
+fn double_thru_channel() {
+    use std::sync::mpsc::{self, Receiver, Sender};
+    use std::thread;
+    use std::time;
+
+    println!("Double a number through channel");
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+
+    thread::spawn(move || {
+        let i = rx.recv().unwrap();
+        println!("{} double is {}", i, i * 2);
+    });
+
+    tx.send(2).unwrap();
+    thread::sleep(time::Duration::from_millis(5));
+}
