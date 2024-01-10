7f8c57d6792907b7ef8594af80be6c0637ae6c73
davidfung <davidfung@amgcomputing.com>
2024-01-10 00:01:05 -0800

    Add spawn async tasks

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use std::io::{self, Write};
 const MENU: &str = "
 MENU
 1. Double Through Channel
+2. Spawning Async Tasks
 > ";
 
 fn main() {
-    let id = menu();
-    match id {
-        1 => double_thru_channel(),
-        _ => println!("Bye"),
-    }
     type Task = fn();
-    let g: Task = double_thru_channel;
+    let id = menu();
+    let g: Task;
+    match id {
+        1 => g = double_thru_channel,
+        2 => g = spawn_async_tasks,
+        _ => g = bye,
+    }
     g();
 }
 
@@ -24,11 +26,15 @@ fn menu() -> i32 {
     println!("");
     match s.trim().parse::<i32>() {
         Err(e) => {
-            println!("{}", e);
+            // println!("{}", e);
             return 0;
         }
         Ok(choice) => return choice,
     }
+}
+
+fn bye() {
+    println!("Bye");
 }
 
 fn double_thru_channel() {
@@ -47,3 +53,59 @@ fn double_thru_channel() {
     tx.send(2).unwrap();
     thread::sleep(time::Duration::from_millis(5));
 }
+
+use async_std::io::prelude::*;
+use async_std::net;
+
+fn spawn_async_tasks() {
+//    async_main(); 
+    let requests = vec![
+        ("example.com".to_string(), 80, "/".to_string()),
+        ("www.red-bean.com".to_string(), 80, "/".to_string()),
+        ("en.wikipedia.org".to_string(), 80, "/".to_string()),
+    ];
+
+    let results = async_std::task::block_on(many_requests(requests));
+    for result in results {
+        match result {
+            Ok(response) => println!("{}", response),
+            Err(err) => eprintln!("error: {}", err),
+        }
+    }
+}
+
+// fn async_main() -> std::io::Result<()> {
+//     use async_std::task;
+//     Ok(())
+// }
+
+pub async fn many_requests(requests: Vec<(String, u16, String)>) -> Vec<std::io::Result<String>> {
+    use async_std::task;
+
+    let mut handles = vec![];
+    for (host, port, path) in requests {
+        handles.push(task::spawn_local(async move {cheapo_request(&host, port, &path).await}));
+    }
+    
+    let mut results = vec![];
+    for handle in handles {
+        results.push(handle.await);
+    }
+
+    results
+}
+
+async fn cheapo_request(host: &str, port: u16, path: &str) -> std::io::Result<String> {
+    let mut socket = net::TcpStream::connect((host, port)).await?;
+
+    let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
+
+    socket.write_all(request.as_bytes()).await?;
+    socket.shutdown(net::Shutdown::Write)?;
+
+    let mut response = String::new();
+    socket.read_to_string(&mut response).await?;
+
+    Ok(response)
+}
+
