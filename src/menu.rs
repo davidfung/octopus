4a8ac8aa9369017c441a0cdd7472bb4122a34d6c
davidfung <davidfung@amgcomputing.com>
2024-02-08 20:34:08 -0800

    Add example of menu usage

--- src/menu.rs
+++ src/menu.rs
@@ -1,23 +1,39 @@
+// module usage:
+//
+// use crate::menu::MenuItem;
+//
+// pub fn menu() -> MenuItem {
+//     MenuItem {
+//         task: entry,
+//         desec: "Module Description...",
+//     }
+// }
+//
+// pub fn entry() {
+//     todo!();
+// }
+
 use std::collections::BTreeMap;
 
 pub struct Menu {
-    items: BTreeMap<u32, MenuItem>
+    items: BTreeMap<u32, MenuItem>,
 }
+
 pub struct MenuItem {
     pub task: fn(),
     pub desc: &'static str,
 }
 
-// pub type Task = fn();
-
 impl Menu {
     pub fn new() -> Menu {
-        Menu{items: BTreeMap::new()}
+        Menu {
+            items: BTreeMap::new(),
+        }
     }
 
     pub fn add_item(&mut self, id: u32, item: MenuItem) {
         self.items.insert(id, item);
-    } 
+    }
 
     pub fn show(&self) {
         for (id, item) in &self.items {
@@ -33,4 +49,3 @@ impl Menu {
         }
     }
 }
-
